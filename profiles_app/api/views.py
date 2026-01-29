e199dac08ed139b2bedc6716f5272a9af1124a78
Kutup Cevik <150686661+KutupCevik@users.noreply.github.com>
2026-01-29 10:50:57 +0100

    add BusinessProfileListView

--- profiles_app/api/views.py
+++ profiles_app/api/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from profiles_app.models import UserProfile
-from .serializers import ProfileSerializer
+from .serializers import ProfileSerializer, ProfileListSerializer
 from .permissions import IsProfileOwnerOrReadOnly
 
 
@@ -9,3 +9,11 @@ class ProfileDetailView(generics.RetrieveUpdateAPIView):
     queryset = UserProfile.objects.select_related("user").all()
     serializer_class = ProfileSerializer
     permission_classes = [IsAuthenticated, IsProfileOwnerOrReadOnly]
+
+
+class BusinessProfileListView(generics.ListAPIView):
+    serializer_class = ProfileListSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return UserProfile.objects.select_related("user").filter(type="business")
