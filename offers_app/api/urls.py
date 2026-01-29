d2fb6169beda810ee4184eefd2decd8ec4a10ede
Kutup Cevik <150686661+KutupCevik@users.noreply.github.com>
2026-01-29 13:23:08 +0100

    Update urls.py

--- offers_app/api/urls.py
+++ offers_app/api/urls.py
@@ -1,3 +1,11 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import OfferViewSet, OfferDetailViewSet
 
-urlpatterns = []
+router = DefaultRouter()
+router.register(r"offers", OfferViewSet, basename="offer")
+router.register(r"offerdetails", OfferDetailViewSet, basename="offerdetail")
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
