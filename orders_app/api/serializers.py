c2dc172ac865941363fd21da974f8907885a2dc2
Kutup Cevik <150686661+KutupCevik@users.noreply.github.com>
2026-01-29 13:55:57 +0100

    add OrderSerializer

---
+++ orders_app/api/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from orders_app.models import Order
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    """Full serializer for listing and retrieving orders."""
+    class Meta:
+        model = Order
+        fields = [
+            "id",
+            "customer_user",
+            "business_user",
+            "title",
+            "revisions",
+            "delivery_time_in_days",
+            "price",
+            "features",
+            "offer_type",
+            "status",
+            "created_at",
+            "updated_at",
+        ]
+        read_only_fields = fields
+
