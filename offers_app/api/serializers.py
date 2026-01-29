19e315b13b1b944ca1b45551d0b0087975af293e
Kutup Cevik <150686661+KutupCevik@users.noreply.github.com>
2026-01-29 12:11:10 +0100

    add OfferListSerializer

--- offers_app/api/serializers.py
+++ offers_app/api/serializers.py
@@ -20,3 +20,32 @@ class OfferDetailLinkSerializer(serializers.ModelSerializer):
     def get_url(self, obj):
         return reverse("offerdetail-detail", kwargs={"pk": obj.id})
 
+
+class OfferListSerializer(serializers.ModelSerializer):
+    min_price = serializers.FloatField(read_only=True)
+    min_delivery_time = serializers.IntegerField(read_only=True)
+    details = OfferDetailLinkSerializer(many=True, read_only=True)
+    user_details = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Offer
+        fields = [
+            "id",
+            "user",
+            "title",
+            "image",
+            "description",
+            "created_at",
+            "updated_at",
+            "details",
+            "min_price",
+            "min_delivery_time",
+            "user_details",
+        ]
+
+    def get_user_details(self, obj):
+        return {
+            "first_name": obj.user.first_name or "",
+            "last_name": obj.user.last_name or "",
+            "username": obj.user.username,
+        }
