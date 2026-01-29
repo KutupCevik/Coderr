0f56893a2082e64c3246862bd42fd723bb942817
Kutup Cevik <150686661+KutupCevik@users.noreply.github.com>
2026-01-29 10:52:00 +0100

    Update urls.py

--- profiles_app/api/urls.py
+++ profiles_app/api/urls.py
@@ -1,3 +1,8 @@
 from django.urls import path
+from .views import ProfileDetailView, BusinessProfileListView, CustomerProfileListView
 
-urlpatterns = []
+urlpatterns = [
+    path("profile/<int:pk>/", ProfileDetailView.as_view(), name="profile-detail"),
+    path("profiles/business/", BusinessProfileListView.as_view(), name="profiles-business"),
+    path("profiles/customer/", CustomerProfileListView.as_view(), name="profiles-customer"),
+]
