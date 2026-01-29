a0134fd22f5d5751d7e49148d89850dbb1bf8d2d
Kutup Cevik <150686661+KutupCevik@users.noreply.github.com>
2026-01-29 10:48:21 +0100

    Add ProfileSerializer

---
+++ profiles_app/api/serializers.py
@@ -0,0 +1,59 @@
+from pathlib import Path
+from rest_framework import serializers
+from profiles_app.models import UserProfile
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    user = serializers.IntegerField(source="user.id", read_only=True)
+    username = serializers.CharField(source="user.username", read_only=True)
+
+    first_name = serializers.CharField(source="user.first_name", required=False, allow_blank=True)
+    last_name = serializers.CharField(source="user.last_name", required=False, allow_blank=True)
+    email = serializers.EmailField(source="user.email", required=False)
+
+    file = serializers.SerializerMethodField()
+
+    class Meta:
+        model = UserProfile
+        fields = [
+            "user",
+            "username",
+            "first_name",
+            "last_name",
+            "file",
+            "location",
+            "tel",
+            "description",
+            "working_hours",
+            "type",
+            "email",
+            "created_at",
+        ]
+        read_only_fields = ["type", "created_at"]
+
+    def get_file(self, obj):
+        if not obj.file:
+            return None
+        return Path(obj.file.name).name
+
+    def update(self, instance, validated_data):
+        user_data = validated_data.pop("user", {})
+        user = instance.user
+
+        for attr in ["first_name", "last_name", "email"]:
+            if attr in user_data:
+                setattr(user, attr, user_data[attr])
+        user.save()
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+
+        return instance
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        for field in ["first_name", "last_name", "location", "tel", "description", "working_hours"]:
+            if data.get(field) is None:
+                data[field] = ""
+        return data
