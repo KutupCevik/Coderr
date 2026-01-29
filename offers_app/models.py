4c6c57f609fecaf1f2d06a332cf5224823277326
Kutup Cevik <150686661+KutupCevik@users.noreply.github.com>
2026-01-29 12:07:21 +0100

    add Offer

--- offers_app/models.py
+++ offers_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Offer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="offers")
+    title = models.CharField(max_length=255)
+    image = models.FileField(upload_to="offers/", null=True, blank=True)
+    description = models.TextField(blank=True, default="")
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
